0153e680e5598326230ae29ef5be695f3ec2265b
sra <sandra.rasoamiaramanana@ledger.fr>
2026-06-03 13:25:34 +0200

    Apply clang-format

--- src/bolos/endorsement.c
+++ src/bolos/endorsement.c
@@ -305,9 +305,10 @@ bolos_err_t sys_ENDORSEMENT_KEY1_SIGN_DATA(uint8_t *data, size_t data_length,
   return ret;
 }
 
-bolos_err_t sys_ENDORSEMENT_KEY1_SIGN_WITHOUT_CODE_HASH(
-    uint8_t *data, size_t data_length, uint8_t *out_signature,
-    size_t *out_signature_length)
+bolos_err_t
+sys_ENDORSEMENT_KEY1_SIGN_WITHOUT_CODE_HASH(uint8_t *data, size_t data_length,
+                                            uint8_t *out_signature,
+                                            size_t *out_signature_length)
 {
   uint32_t tmp_len = *out_signature_length;
   bolos_err_t ret = sys_ENDORSEMENT_key1_sign_without_code_hash(
